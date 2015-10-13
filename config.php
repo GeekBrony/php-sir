8e470d63ba3c84fab1933a4797d53a1036a295bf
GeekBrony <x86cam@gmail.com>
2015-10-13 13:55:09 -0500

    More images added

--- config.php
+++ config.php
@@ -8,7 +8,13 @@ $images = array(
   0 => 'http://lunachan.net/site/src/138996621296.gif',
   1 => 'http://orig15.deviantart.net/0a17/f/2013/226/e/7/untitled_by_lloserlife-d6i3dv8.gif',
   2 => 'http://www.bronydom.net/img/derp.gif',
-  3 => 'http://orig08.deviantart.net/1785/f/2012/188/4/4/derpy_loves_muffins__by_tomdantherock-d56cv33.gif'
-);
+  3 => 'http://orig08.deviantart.net/1785/f/2012/188/4/4/derpy_loves_muffins__by_tomdantherock-d56cv33.gif',
+  4 => 'http://vignette4.wikia.nocookie.net/poptropica/images/8/8a/Derpy_Milkshake.gif',
+  5 => 'http://orig15.deviantart.net/02b8/f/2012/313/5/4/derpy_bop_by_blackgryph0n-d5kiv63.gif',
+  6 => 'http://orig11.deviantart.net/6730/f/2012/049/e/5/paddle_ball_by_subjectnumber2394-d4q3nal.gif'
+  7 => 'http://orig07.deviantart.net/c00a/f/2012/359/8/6/doodoodoo_gif___derpy_by_1n33d4hug-d5p4ioc.gif',
+  8 => 'http://orig09.deviantart.net/50ed/f/2011/332/f/7/derpy_hovering_test_by_c_quel-d4hmz2x.gif',
+  9 => 'http://pinkie.mylittlefacewhen.com/media/f/img/mlfw9201-210545.gif',
+); // because Derpy is best pony
 
 ?>
